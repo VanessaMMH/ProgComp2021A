f6ae2784c1774ed977d0630e45df8ec29a1a1295
mahua <mahua.ea@gmail.com>
2021-10-24 21:45:21 -0500

    921. Minimum Add to Make Parentheses Valid

--- Aula10/Ejercicio2_minAddToMakeValid.cpp
+++ Aula10/Ejercicio2_minAddToMakeValid.cpp
@@ -7,21 +7,22 @@ link: https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
 */
 
 int minAddToMakeValid(string s) {
-    stack<int> pila;
+    stack<char> pila;
+    bool flag;
     for (size_t i = 0; i < s.size(); i++)
     {
-        if (s[i]=='(' && s[i+1]==')' )
-            i++;
+        if (s[i]==')' && pila.size()>0 && pila.top()=='(')
+            pila.pop();
         else
-            pila.push(s[i]);       
-    }
+            pila.push(s[i]);                  
+    }   
     return pila.size();   
 }
 
 int main(int argc, char const *argv[])
 {
     // string s ="())";
-    string s ="()))((";
+    string s ="((())";
     cout << minAddToMakeValid( s);
     return 0;
 }
